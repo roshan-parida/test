4a22cc83eda3ac8d9a9376db2dbf0cbc6d51d295
Roshan Parida <roshanparida24@gmail.com>
2026-01-08 13:43:19 +0530

    modified: CORS origins

--- src/main.ts
+++ src/main.ts
@@ -35,9 +35,23 @@ async function bootstrap() {
 	});
 
 	app.enableCors({
-		origin: ['http://localhost:3000', process.env.FRONTEND_URL].filter(
-			Boolean,
-		),
+		origin: (
+			origin: string | undefined,
+			callback: (err: Error | null, allow?: boolean) => void,
+		) => {
+			const allowedOrigins = [
+				'http://localhost:3000',
+				process.env.FRONTEND_URL,
+			].filter(Boolean);
+
+			if (!origin || allowedOrigins.includes(origin)) {
+				callback(null, true);
+			} else {
+				callback(new Error('Not allowed by CORS'));
+			}
+		},
+		methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+		allowedHeaders: ['Content-Type', 'Authorization'],
 		credentials: true,
 	});
 
